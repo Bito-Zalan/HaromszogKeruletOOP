aaa3760f33cbe462ed77ab5014a7f15eb7c7ff31
Bito-Zalan <bitozalan23@gmail.com>
2024-04-17 20:41:25 +0200

    Hsz ker elkezdve

---
+++ src/main/Haromszog.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Random;
+
+
+public class Haromszog {
+
+    private static final Random rnd = new Random();
+    private double a,b,c;
+    
+    public double kerulet(){
+        if
+    }
+    
+}
